8b354162a8dd13e46eee4cefd892170c9741fbd6
nasif-jaoadun <nasif_online@yahoo.com>
2023-08-20 17:32:04 +0600

    Trace lifecycle events by logcat

--- app/src/main/java/com/codehabit/androidapparchitecture/MainActivity.kt
+++ app/src/main/java/com/codehabit/androidapparchitecture/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.codehabit.androidapparchitecture
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.codehabit.androidapparchitecture.ui.LOG_TAG
 import com.codehabit.androidapparchitecture.ui.theme.AndroidAppArchitectureTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,6 +28,31 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(LOG_TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(LOG_TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(LOG_TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(LOG_TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(LOG_TAG, "onDestroy")
     }
 }
 
---
+++ app/src/main/java/com/codehabit/androidapparchitecture/ui/Constants.kt
@@ -0,0 +1,3 @@
+package com.codehabit.androidapparchitecture.ui
+
+const val LOG_TAG = "Android APP Architecture"
